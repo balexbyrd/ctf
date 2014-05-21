942866bf69f6b4419e921c01e56eea7d1e97ec41
lawmicha <mlaw135@gmail.com>
2014-05-20 23:06:03 -0400

    ch1 done

--- Violent Python/Companion Files/CH1/1-vulnScanner.py
+++ Violent Python/Companion Files/CH1/1-vulnScanner.py
@@ -13,7 +13,9 @@ def retBanner(ip, port):
         banner = s.recv(1024)
         return banner
     except:
+        print 'No banner for '+str(ip)+','+str(port)
         return
+
 
 
 def checkVulns(banner, filename):
@@ -42,7 +44,7 @@ def main():
         print '[-] Usage: ' + str(sys.argv[0]) +\
             ' <vuln filename>'
         exit(0)
-
+    print 'Ready'  
     portList = [21,22,25,80,110,443]
     for x in range(147, 150):
         ip = '192.168.95.' + str(x)
