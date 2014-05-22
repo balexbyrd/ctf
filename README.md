dd77871f7bfb249129a5c535b6e6dc3471c577d2
lawmicha <mlaw135@gmail.com>
2014-05-22 10:30:33 -0400

    ch2 stuff partial

---
+++ README.md
@@ -0,0 +1,11 @@
+CH1
+hosterbox ftp server 184.22.118.62:2082
+
+
+CH2
+pretty printing json
+print json.dumps(<json>,  sort_keys=True, indent=4, separators=(',',':'))
+
+free ssh account
+ssh lawmicha@sdf.org
+pass: compsex
--- Violent Python/Companion Files/CH2/3-sshCommand.py
+++ Violent Python/Companion Files/CH2/3-sshCommand.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import pexpect
 
-PROMPT = ['# ', '>>> ', '> ','\$ ']
+#we inclue the pexpect.TIMEOUT in here incase we don't get a prompt
+PROMPT = [pexpect.TIMEOUT,'# ', '>>> ', '> ','\$ ']
 
 def send_command(child, cmd):
     child.sendline(cmd)
@@ -16,6 +17,7 @@ def connect(user, host, password):
     ret = child.expect([pexpect.TIMEOUT, ssh_newkey,\
                         '[P|p]assword:'])
     
+    print 'ret = '+str(ret)
     if ret == 0:
         print '[-] Error Connecting'
         return
@@ -27,16 +29,29 @@ def connect(user, host, password):
         if ret == 0:
             print '[-] Error Connecting'
             return
-    
+
+
+    #ret == 2, we send the password
     child.sendline(password)
-    child.expect(PROMPT)
+
+    #check for prompt
+    ret2 = child.expect(PROMPT)
+    while ret2 == 0:
+        print 'NO PROMPT ret2 = '+str(ret2)
+        child.sendline(" ")
+        ret2 = child.expect(PROMPT)
+
+    print 'PROMPT FOUND'
     return child
 
 
 def main():
-    host = 'localhost'
-    user = 'root'
-    password = 'toor'
+    #host = 'localhost'
+    #user = 'root'
+    #password = 'toor'
+    host = 'sdf.org'
+    user = 'lawmicha'
+    password = 'compsex'
     
     child = connect(user, host, password)
     send_command(child, 'cat /etc/shadow | grep root')
